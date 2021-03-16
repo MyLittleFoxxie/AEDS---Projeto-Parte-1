6194cc06c83129a19f656efc511f5d186d032d55
MyLittleFoxxie <78807369+MyLittleFoxxie@users.noreply.github.com>
2021-03-16 20:27:26 -0300

    Update Main.cpp

--- Projeto_Parte_1/Main.cpp
+++ Projeto_Parte_1/Main.cpp
@@ -8,31 +8,6 @@ Data:19/03/21
 
 ****************/
 
-//Estamos buscando a sequência 1 3 2 3 1 em um arquivo texto
-#include <stdio.h>
-
-int main () 
-{
-    FILE *arquivo;
-    char nomeArquivo[100];
-    int qtdItens;
-    int elementosLista[qtdItens];
-
-	printf("Digite o nome do arquivo: ");
-	scanf("%s", nomeArquivo);
-	arquivo = fopen(nomeArquivo, "r");
-
-    //Escaneia a quantidade de itens
-	fscanf(arquivo, "%d", &qtdItens);
-
-    //Escaneia cada elemento e insere no vetor
-	for (int i = 0; i < qtdItens; i++)
-		fscanf(arquivo, "%d", &elementosLista[i]);
-
-    procurarPadrao(elementosLista, qtdItens);
-}
-
-
 void procurarPadrao(int* elementosLista, int tamanho)
 {
     int padrao = 0;
@@ -127,4 +102,29 @@ void procurarPadrao(int* elementosLista, int tamanho)
         printf("\nResultado: Padrao nao encontrado.\n");
         break;
     }
+}
+
+
+//Estamos buscando a sequência 1 3 2 3 1 em um arquivo texto
+#include <stdio.h>
+
+int main () 
+{
+    FILE *arquivo;
+    char nomeArquivo[100];
+    int qtdItens;
+    int elementosLista[qtdItens];
+
+	printf("Digite o nome do arquivo: ");
+	scanf("%s", nomeArquivo);
+	arquivo = fopen(nomeArquivo, "r");
+
+    //Escaneia a quantidade de itens
+	fscanf(arquivo, "%d", &qtdItens);
+
+    //Escaneia cada elemento e insere no vetor
+	for (int i = 0; i < qtdItens; i++)
+		fscanf(arquivo, "%d", &elementosLista[i]);
+
+    procurarPadrao(elementosLista, qtdItens);
 }
