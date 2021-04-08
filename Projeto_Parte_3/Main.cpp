4747d852b9705f17a7d74f92344ddab599e00d68
Gabriel Bernalle <gabrielbbmatozinhos@gmail.com>
2021-04-08 19:42:44 -0300

    Código funcionando sem lista encadeada

--- Projeto_Parte_3/Main.cpp
+++ Projeto_Parte_3/Main.cpp
@@ -2,76 +2,14 @@
 LAED1 - Projeto Parte 2
 Alunos(as): Vitor Brandão Raposo e Gabriel Bernalle
 Data: 26/03/21
+
 ****************/
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/time.h>
-#define MAX 10
-
-/* ========================================================================= */
-
-typedef int TipoChave;
-typedef struct {
-  int Chave;
-  /* outros componentes */
-} TipoItem;
-
-typedef struct TipoCelula *TipoApontador;
-
-typedef struct TipoCelula {
-  TipoItem Item;
-  TipoApontador Prox;
-} TipoCelula;
-
-typedef struct {
-  TipoApontador Primeiro, Ultimo;
-} TipoLista;
-
-/* ========================================================================= */
-
-void FLVazia(TipoLista *Lista)
-{ Lista -> Primeiro = (TipoApontador) malloc(sizeof(TipoCelula));
-  Lista -> Ultimo = Lista -> Primeiro;
-  Lista -> Primeiro -> Prox = NULL;
-}
-
-int Vazia(TipoLista Lista)
-{ return (Lista.Primeiro == Lista.Ultimo);
-}
-
-void Insere(TipoItem x, TipoLista *Lista)
-{ Lista -> Ultimo -> Prox = (TipoApontador) malloc(sizeof(TipoCelula));
-  Lista -> Ultimo = Lista -> Ultimo -> Prox;
-  Lista -> Ultimo -> Item = x;
-  Lista -> Ultimo -> Prox = NULL;
-}
-
-void Retira(TipoApontador p, TipoLista *Lista, TipoItem *Item)
-{ /*  ---   Obs.: o item a ser retirado e  o seguinte ao apontado por  p --- */
-  TipoApontador q;
-  if (Vazia(*Lista) || p == NULL || p -> Prox == NULL) 
-  { printf(" Erro   Lista vazia ou posi  c   a o n  a o existe\n");
-    return;
-  }
-  q = p -> Prox;
-  *Item = q -> Item;
-  p -> Prox = q -> Prox;
-  if (p -> Prox == NULL) Lista -> Ultimo = p;
-  free(q);
-}
-
-void Imprime(TipoLista Lista)
-{ TipoApontador Aux;
-  Aux = Lista.Primeiro -> Prox;
-  while (Aux != NULL) 
-    { printf("%d\n", Aux -> Item.Chave);
-      Aux = Aux -> Prox;
-    }
-}
-/* ========================================================================== */
 
 //Estamos buscando a sequência “1 3 1 3 1 3 1 3 1 3 1 3 1" nessa ordem
-void procurarPadrao(int* elementosLista, int qtdItens)
+bool procurarPadrao(int* elementosLista, int qtdItens)
 {
     int padrao = 0, resultado = 0;
 
@@ -105,15 +43,17 @@ void procurarPadrao(int* elementosLista, int qtdItens)
         }
     }
     
-    if (resultado == 1)
-        printf("Resultado: Padrao de faixa de pedestres encontrado.\n");
-
-    else
-        printf("Resultado: Padrao de faixa de pedestres nao encontrado.\n");       
+    if (resultado == 1){
+    	printf("Resultado: Pista com faixa de pedestres\n");
+		return true;
+	}
+    else{
+		return false;
+	}      
 }
 
 
-void mapeiaPadrao(int* elementosLista, int qtdItens)
+bool mapeiaPadrao(int* elementosLista, int qtdItens)
 {
 	int listaFinal[qtdItens];
 
@@ -123,11 +63,13 @@ void mapeiaPadrao(int* elementosLista, int qtdItens)
 		if(elementosLista[i] == 255) listaFinal[i] = 3; 
 	}
 
-	procurarPadrao(listaFinal, qtdItens);
+	bool padrao = procurarPadrao(listaFinal, qtdItens);
+	if (padrao == true) return true;
+	else return false;
 }
 
 
-void reduzPadrao(int* elementosLista, int qtdItens)
+bool reduzPadrao(int* elementosLista, int qtdItens)
 {
 	int *auxList;
 	auxList = (int*) malloc(qtdItens * sizeof (int));
@@ -142,90 +84,52 @@ void reduzPadrao(int* elementosLista, int qtdItens)
 			aux = elementosLista[i];
 		}
 	}
-
+	
     qtdItens = j + 1;
 
-	mapeiaPadrao(auxList, qtdItens);
+	bool padrao = mapeiaPadrao(auxList, qtdItens);
+
+	if(padrao == true) return true;
+	else return false;
 }
 
 
 int main () 
 {
-	struct timeval t;
+//	struct timeval t;
 
     FILE *arquivo;
     char nomeArquivo[100];
-    int qtdItens;
+    int qtdlinhas;
+	int qtdItens;
 
-/*	printf("Digite o nome do arquivo: ");
-	scanf("%s", nomeArquivo);
-	arquivo = fopen(nomeArquivo, "r");
+	printf("Digite o nome do arquivo: ");
+	  scanf("%s", nomeArquivo);
+	  arquivo = fopen(nomeArquivo, "r");
 
     if (arquivo == NULL){
         printf("\nERROR: IMPOSSIBLE TO OPEN THE FILE");
         exit(1);
     }
 
+	bool padrao;
     //Escaneia a quantidade de itens
-	fscanf(arquivo, "%d", &qtdItens);
+	fscanf(arquivo, "%d", &qtdlinhas);
 
-    int elementosLista[qtdItens];
+	for(int i = 0; i < qtdlinhas; i++){
+		// Escaneia a quantidade de Linhas
+    	fscanf(arquivo, "%d", &qtdItens);
+		int elementosLista[qtdItens];
 
-    //Escaneia cada elemento e insere no vetor
-	for (int i = 0; i < qtdItens; i++)
-    {    
-        fscanf(arquivo, "%d", &elementosLista[i]);
-    }
+    	//Escaneia cada elemento e insere no vetor
+	  	for (int j = 0; j < qtdItens; j++) fscanf(arquivo, "%d", &elementosLista[j]);
 
-	reduzPadrao(elementosLista,qtdItens);
+	  		padrao = reduzPadrao(elementosLista,qtdItens);
+		if(padrao == true) break;
+	}
+	if(padrao == false) printf("Resultado: Pista sem faixa de pedestres\n");
 
     fclose(arquivo);
-*/
-
-	TipoLista lista;
-  	TipoItem item;
-  	int vetor[MAX];
-	TipoApontador p;
-  	int i, j, k, n;
-  	float  tamanho=0;
-  	gettimeofday(&t,NULL);
-  	srand((unsigned int)t.tv_usec);
-  	FLVazia(&lista);
-
-  
-  	/*Gera uma permutacao aleatoria de chaves entre 1 e MAX*/
-  	for(i = 0; i < MAX; i++) vetor[i] = i + 1;
-  	for(i = 0; i < MAX; i++){ 
-		k =  (int) (10.0 * rand()/(RAND_MAX + 1.0));
-      	j =  (int) (10.0 * rand()/(RAND_MAX + 1.0));
-      	n = vetor[k];
-      	vetor[k] = vetor[j];
-      	vetor[j] = n;
-    }
-
-  	/*Insere cada chave na lista */
-  	for (i = 0; i < MAX; i++)
-    {
-		item.Chave = vetor[i];
-      	Insere(item, &lista);
-      	tamanho++;
-      	printf("Inseriu: %d \n", item.Chave);
-    }
-  	
-	Imprime(lista);
-
-  	/*Retira cada chave da lista */
-  	for(i = 0; i < MAX; i++)
-    { /*escolhe uma chave aleatoriamente */
-    	k = (int) ((tamanho) * rand() / (RAND_MAX + 1.0));
-      	p = lista.Primeiro;
-      	/*retira chave apontada */
-      	Retira(p, &lista, &item);
-      	tamanho--;
-      	printf("Retirou: %d\n", item.Chave);
-    }
-  	
-	Imprime (lista);
-  	
+	
 	return(0);
 }
